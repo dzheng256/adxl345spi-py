389b074479d4dc6574d9c83ed57fc5d2a7d0996a
Daniel Zheng <dzheng256@gmail.com>
2018-11-08 01:21:00 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='adxl345',
+    version='0.1',
+    packages=['adxl345',],
+    license='GPL-3.0',
+    long_description=open('README.md').read(),
+)
